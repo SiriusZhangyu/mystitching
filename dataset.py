341cc43729d90aa6010c1aeffd5502d163dd42b5
SiriusZhangyu <43713383+SiriusZhangyu@users.noreply.github.com>
2021-08-17 11:48:02 -0400

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -22,12 +22,12 @@ class mydataset(data.Dataset):
 
     def __getitem__(self, index):
         img_lists=os.listdir(self.img1_path)
-        img1 = cv2.imread(os.path.join(self.img1_path,img_lists[index]),cv2.IMREAD_GRAYSCALE)
+        img1 = cv2.imread(os.path.join(self.img1_path,img_lists[index]),0)
 
-        img2 = cv2.imread(os.path.join(self.img2_path, img_lists[index]),cv2.IMREAD_GRAYSCALE)
+        img2 = cv2.imread(os.path.join(self.img2_path, img_lists[index]),0)
 
-        mask1=cv2.imread(os.path.join(self.mask1_path,img_lists[index]),cv2.IMREAD_GRAYSCALE)
-        mask2=cv2.imread(os.path.join(self.mask2_path,img_lists[index]),cv2.IMREAD_GRAYSCALE)
+        mask1=cv2.imread(os.path.join(self.mask1_path,img_lists[index]),0)
+        mask2=cv2.imread(os.path.join(self.mask2_path,img_lists[index]),0)
 
         img1 = torch.Tensor(img1)
         img1 = img1.unsqueeze(0)
@@ -35,10 +35,9 @@ class mydataset(data.Dataset):
         img2 = torch.Tensor(img2)
         img2 = img2.unsqueeze(0)
 
+        ret, mask1 = cv2.threshold(mask1, 127, 1, cv2.THRESH_BINARY)
         mask1 = torch.Tensor(mask1)
-        mask1 = mask1.unsqueeze(0)
 
         mask2 = torch.Tensor(mask2)
-        mask2 = mask2.unsqueeze(0)
 
         return img1, img2, mask1, mask2
