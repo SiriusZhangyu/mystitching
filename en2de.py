bc94644e80d14a6b5f256195531edff6c75cbcde
SiriusZhangyu <43713383+SiriusZhangyu@users.noreply.github.com>
2021-08-23 16:40:36 -0400

    Update en2de.py

--- en2de.py
+++ en2de.py
@@ -1,87 +1,88 @@
+
+
 import os
-import cv2
-import math
 import json
-import random
-import numpy as np
 import torch
-import torchvision
 import torch.nn as nn
-import torch.nn.functional as F
+from torchvision import transforms, datasets, utils
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.optim as optim
+from tqdm import tqdm
+from dataset import mydataset
+import torchvision.models as models
+from en2de import Matcher
+import en2de
+
+torch.cuda.empty_cache()
+
+def main():
+    torch.cuda.empty_cache()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("using {} device.".format(device))
+
+    source_root='./source'
+    sys_root='./sys_img'
+    source_gt_root='./source_gt'
+    sys_gt_root='./sys_gt'
 
 
+    train_dataset = mydataset(img1_path=source_root, img2_path=sys_root, mask1_path=source_gt_root,mask2_path=sys_gt_root)
 
-class _DecoderBlock(nn.Module):
-    def __init__(self, in_channels, middle_channels, out_channels):
-        super(_DecoderBlock, self).__init__()
-        self.decode = nn.Sequential(
-            nn.Conv2d(in_channels, middle_channels, kernel_size=3),
-            nn.BatchNorm2d(middle_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(middle_channels, middle_channels, kernel_size=3),
-            nn.BatchNorm2d(middle_channels),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(middle_channels, out_channels, kernel_size=2, stride=2),
-        )
+    batch_size = 1
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size, shuffle=True,
+                                               num_workers=0)
 
-    def forward(self, x):
-        return self.decode(x)
+    encoder = en2de.Encoder(models.__dict__['resnet50'])
+    encoder.load_state_dict(torch.load('rotationinvariantmodel.pth'))
 
 
+    net = Matcher()
+    net.to(device)
+    epochs = 1
+    save_path = 'en2de.pth'
+    optimizer = optim.Adam(net.parameters(), lr=0.0002)
 
-class Matcher(nn.Module):
-    def __init__(self):
-        super(Matcher, self).__init__()
-        # self.encoder = torchvision.models.__dict__['resnet50'](pretrained=True)
-        self.eps = 1e-8
-        self.num_classes = 2
-        self.encoder = torch.load('rotationinvariantmodel.pth')
-        for param in self.encoder :
-            param.requires_grad = False
+    _loss = nn.CrossEntropyLoss()
 
-        self.enc1 = self.encoder.layer3
-        self.enc2 = self.encoder.layer4
+    Loss_list = []
 
+    for epoch in range(epochs):
+        # train
+        net.train()
+        train_bar = tqdm(train_loader)
+        l_sum=0.0
+        for step, data in enumerate(train_bar):
+            source_img, sys_img,source_gt,sys_gt = data
+            optimizer.zero_grad()
 
-        self.dec4 = _DecoderBlock(6144,3072, 1536)
-        self.dec3 = _DecoderBlock(1536, 768, 384)
-        self.dec2 = _DecoderBlock(384, 192, 96)
-        self.dec1 = nn.Conv2d(96, self.num_classes, kernel_size=1)
+            x1,x11,x2,x22 =encoder(source_img,sys_img)
 
-    def nn_concat(self, src_feat, tar_feat):
-        """
-        For each pixel in source feature, we find its nearest feature vector in the target feature and concatenate them.
-        src_feat: B*C*H1*W1
-        tar_feat: B*C*H2*W2
-        concat_feat: B*2C*H1*W1
-        """
-        src_feat_flat = src_feat.flatten(start_dim=2).transpose(2,1)
-        tar_feat_flat = tar_feat.flatten(start_dim=2).transpose(2,1)
-        dist_mat = torch.cdist(src_feat_flat, tar_feat_flat)
-        tar_idx = dist_mat.argmin(dim=2)
-        nearest_tar_feat = torch.zeros_like(src_feat_flat)
-        for b in range(src_feat.shape[0]):
-            nearest_tar_feat[b] = tar_feat_flat[b][tar_idx[b]]
-        nearest_tar_feat = nearest_tar_feat.transpose(2,1).reshape(*src_feat.shape)
-        concat_feat = torch.cat((src_feat, nearest_tar_feat), 1)
-        return concat_feat
+            output=net(source_img.to(device), x1.to(device),x11.to(device),x2.to(device),x22.to(device))
 
-    def forward(self, src_img, tar_img):
-        src_feat1 = self.enc1(src_img)
-        src_feat2 = self.enc2(src_feat1)
+            loss=_loss(output , source_gt.to(device, dtype=torch.int64))
+
+            loss.backward()
+            optimizer.step()
+            l_sum += loss
+
+        Loss_list.append(l_sum)
 
 
-        tar_feat1 = self.enc1(tar_img)
-        tar_feat2 = self.enc2(tar_feat1)
+        net.eval()
+
+    x=range(0,100)
+    plt.plot(x, Loss_list, 'o-')
+    plt.title('Test loss vs. epoches')
+    plt.ylabel('Test loss')
+    plt.savefig('loss.png')
+
+    torch.save(net, save_path)
+
+    print('Finished Training')
 
 
-        concat_feat1 = self.nn_concat(src_feat1, tar_feat1)
-        concat_feat2 = self.nn_concat(src_feat2, tar_feat2)
-
-        dec_feat = self.dec4(torch.cat([concat_feat1, F.interpolate(concat_feat2, concat_feat1.size()[2:], mode='bilinear')], 1))
-        dec_feat = self.dec3(dec_feat)
-        dec_feat = self.dec2(dec_feat)
-        dec_feat = self.dec1(dec_feat)
-        pred_mask = F.upsample(dec_feat, src_img.size()[2:], mode='bilinear')
-
-        return pred_mask
+if __name__ == '__main__':
+    torch.cuda.empty_cache()
+    main()
